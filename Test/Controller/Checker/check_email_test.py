d5f85ce9daed6a82191759b5fada9ded8cb79b45
VasilevSaryal <vasilyev.s@indriver.com>
2020-12-14 22:47:10 +0900

    Add comment check email

--- Test/Controller/Checker/check_email_test.py
+++ Test/Controller/Checker/check_email_test.py
@@ -23,12 +23,14 @@ def check_email(email):
 
 class CheckEmailTestCase(unittest.TestCase):
     def test_check_email(self):
+        # given
         list_emails = ("vladimirp1998@gmail.com", "eremenkoira19981998@gmail.com", "fhljkjhuh@gmail.com",
                        "saryal.basilev@gmail.com", "sarasti2400@gmail.com", "evgunarov@gmail.com",
                        "ivanovmksm98@gmail.com", "eavamga@gmail.com", "nikofed96@gmail.com", "ergis15@gmail.com",
                        "maksimbubyakin@mail.ru", "plats2002@gmail.com", "m.fiit2020@gmail.com",
                        "dayankoo201297@gmail.com", "semenovanatalya400180@gmail.com", "arsen3393@gmail.com",
                        "eremenkoira19981998@mail.ru", "maksimzlatov96@gmail.com")
+        # given
         list_wrong_emails = ("vladimirp1998@gmailcom", "eremenkoira19981998@gmail.compsdfsa", "fhljkjhuh",
                              56, "sarasti2400gmail.comsdf", "com.evgunarov@gmail",
                              "ivanovmksm98@gmail.?com", "eavamga@gmail.34com", "nikofed96gmail.co1m",
@@ -36,6 +38,8 @@ class CheckEmailTestCase(unittest.TestCase):
                              "maksimbubyakin@mailru", "plats2002@gmail.90com", "m.fiit2020@gmailcom",
                              True, False, 2134234, 0.13343, (1, "eavamga@gmail.ru.com", 3, 5))
         for i in list_emails:
+            # then
             self.assertEqual(check_email(i), True)
         for j in list_wrong_emails:
+            # then
             self.assertEqual(check_email(j), False)
