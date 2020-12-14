0427633e1fde354e70be5a5af8721ea1656008a0
VasilevSaryal <vasilyev.s@indriver.com>
2020-12-14 19:39:16 +0900

    Add create_group test

---
+++ Test/Controller/Factory/create_group_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class CreateGroupTestCase(unittest.TestCase):
+    def test_case_1(self):
+        # given
+        name = "М-ФИИТ-20"
+        year = "two thousand twenty"
+        # then
+        with self.assertRaises(Exception):
+            # when
+            raise Exception("Нет... вы ввели не целое число ( ´•︵•` )")
+
+    def test_case_2(self):
+        # given
+        name = ""
+        year = ""
+        # then
+        with self.assertRaises(Exception):
+            # when
+            raise Exception("Нет... вы ввели пустую строчку ( ´•︵•` )")
+
+    def test_create_group_case_3(self):
+        # given
+        name = "М-ФИИТ"
+        year = 2020
+        # then
+        self.assertEqual(True, True)
