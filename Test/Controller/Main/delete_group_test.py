d8280de44b65939e95cec804cad5874e6c8506f5
VasilevSaryal <vasilyev.s@indriver.com>
2020-12-18 00:59:08 +0900

    delete group test enhancement

--- Test/Controller/Main/delete_group_test.py
+++ Test/Controller/Main/delete_group_test.py
@@ -3,22 +3,35 @@ import unittest
 from unittest.mock import patch
 
 
-def delete_group_logic(cmd_input: int):
-    if cmd_input == "1":
-        print("Успешно удален\n",
-              "0. Назад")
-    elif cmd_input == "0":
+def convert_input_to_int(user_input: str):
+    try:
+        val = int(user_input)
+        return val
+    except ValueError:
+        raise Exception("Нет... вы ввели не целое число ( ´•︵•` )")
+
+
+def delete_group_logic(cmd_input: str, groups_count: int):
+    if cmd_input == "":
+        raise Exception("Нет... вы ввели пустую строчку ( ´•︵•` )")
+    cmd_input = convert_input_to_int(cmd_input)
+    if cmd_input == 0:
         print("0 - Выход\n",
               "1 - Студент\n",
               "2 - Группа\n",
               "3 - Предмет\n",
               "4 - БРС", sep="")
+    elif cmd_input <= groups_count:
+        print("Успешно удален\n",
+              "0. Назад")
+    else:
+        raise Exception("Выход за пределы массива ( ´•︵•` )")
 
 
 class DeleteGroupTestCase(unittest.TestCase):
     def setUp(self):
         self.groups = [["М-ФИИТ", 2020], ["М-ИВТ", 2020]]
-        self.cmd_input = "1"
+        self.cmd_input = ["1"]
         self.success_output = "Успешно удален\n" \
                               " 0. Назад"
         self.back_output = "0 - Выход\n" \
@@ -32,8 +45,9 @@ class DeleteGroupTestCase(unittest.TestCase):
         # given setUp
         with patch('builtins.input', side_effect=self.cmd_input):
             cmd_input = input()
+            groups_count = len(self.groups)
             # when
-            delete_group_logic(cmd_input)
+            delete_group_logic(cmd_input, groups_count)
 
         result = mock_obj.getvalue().strip()
         # then
@@ -42,43 +56,57 @@ class DeleteGroupTestCase(unittest.TestCase):
     def test_case_1(self):
         # given
         self.groups = []
-        # then
-        with self.assertRaises(Exception):
-            # when
-            raise Exception("Выход за пределы массива ( ´•︵•` )")
+        with patch('builtins.input', side_effect=self.cmd_input):
+            cmd_input = input()
+            groups_count = len(self.groups)
+            # then
+            with self.assertRaises(Exception):
+                # when
+                raise delete_group_logic(cmd_input, groups_count)
 
     def test_case_2(self):
         # given
-        self.cmd_input = "3"
-        # then
-        with self.assertRaises(Exception):
-            # when
-            raise Exception("Выход за пределы массива ( ´•︵•` )")
+        self.cmd_input = ["3"]
+        with patch('builtins.input', side_effect=self.cmd_input):
+            cmd_input = input()
+            groups_count = len(self.groups)
+            # then
+            with self.assertRaises(Exception):
+                # when
+                raise delete_group_logic(cmd_input, groups_count)
 
     def test_case_3(self):
         # given
-        self.cmd_input = "two"
-        # then
-        with self.assertRaises(Exception):
-            # when
-            raise Exception("Нет... вы ввели не целое число ( ´•︵•` )")
+        self.cmd_input = ["two"]
+        with patch('builtins.input', side_effect=self.cmd_input):
+            cmd_input = input()
+            groups_count = len(self.groups)
+            # then
+            with self.assertRaises(Exception):
+                # when
+                raise delete_group_logic(cmd_input, groups_count)
 
     def test_case_4(self):
         # given
-        self.cmd_input = ""
-        # then
-        with self.assertRaises(Exception):
-            # when
-            raise Exception("Нет... вы ввели пустую строчку ( ´•︵•` )")
+        self.cmd_input = [""]
+        with patch('builtins.input', side_effect=self.cmd_input):
+            cmd_input = input()
+            groups_count = len(self.groups)
+            print(cmd_input)
+            # then
+            with self.assertRaises(Exception):
+                # when
+                raise delete_group_logic(cmd_input, groups_count)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_case_5(self, mock_obj):
         # given
-        self.cmd_input = '0'
+        self.cmd_input = ['0']
         with patch('builtins.input', side_effect=self.cmd_input):
             cmd_input = input()
+            groups_count = len(self.groups)
             # when
-            delete_group_logic(cmd_input)
+            delete_group_logic(cmd_input, groups_count)
         result = mock_obj.getvalue().strip()
         # then
         self.assertEqual(self.back_output, result)
